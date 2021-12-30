c7a916b77f8e0a7fc5e6c851693e054291ae050f
Egor <lelikegor1@gmail.com>
2021-12-30 22:37:22 +0300

    Rework problem

--- task1/Task1.py
+++ task1/Task1.py
@@ -1,26 +1,30 @@
 from itertools import cycle
+import argparse
 
-with open('Task1_n.txt', 'r', encoding='utf8') as n:
-    n0 = n.read()
-    n0 = int(n0)
+parser = argparse.ArgumentParser()
 
-with open('Task1_m.txt', 'r', encoding='utf8') as m:
-    m0 = m.read()
-    m0 = int(m0)
+parser.add_argument("n", type=int, help="Argument n")
+parser.add_argument("m", type=int, help="Argument m")
 
-way = []
-i = 0
-lst = cycle(list(range(1,n0+1)))
-for item in lst:
-    if i == 0:
-        way.append(item)
-    else:
-        if i == m0 - 1:
-            if item in way:
-                break
+args = parser.parse_args()
+
+def task1(n,m):
+
+    way = []
+    i = 0
+    lst = cycle(list(range(1,n+1)))
+    for item in lst:
+        if i == 0:
             way.append(item)
-            i = 0
+        else:
+            if i == m - 1:
+                if item in way:
+                    break
+                way.append(item)
+                i = 0
 
-    i += 1
+        i += 1
 
-print("".join(map(str,way)))
+    print("".join(map(str,way)))
+
+task1(args.n, args.m)
