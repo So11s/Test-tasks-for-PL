c0e361fd73832891f34ee38eea50a59e0918310c
Egor <lelikegor1@gmail.com>
2021-12-30 22:21:40 +0300

    Rework problem

--- task3/Task3.py
+++ task3/Task3.py
@@ -1,26 +1,26 @@
 import json
 
-with open('tests.json', 'r') as file:
+with open('C:/Users/lelik/Desktop/tests1.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-with open('values.json', 'r') as file_2:
+with open('C:/Users/lelik/Desktop/values1.json', 'r', encoding='utf-8') as file_2:
     todos = json.load(file_2)
 
-for k, v in todos.items():
 
-        for i in v:
-
-            for t, y in data.items():
-
-                if i.get('id') == data.get('id'):
-                    data['value'] = i['value']
-
-                elif isinstance(y,list):
-
+def task3(sqr, sqr2):
+    if isinstance(sqr, dict):
+        for k, v in sqr.copy().items():
+            if isinstance(v, list):
+                for i in v:
+                    if isinstance(i, dict):
+                        task3(i, sqr2)
+            else:
+                for t, y in sqr2.items():
                     for elem in y:
+                        if sqr['id'] == elem['id'] and sqr.get('value') == '':
+                            sqr['value'] = elem['value']
 
-                        if i.get('id') == elem.get('id'):
-                            elem['value'] = i['value']
+    with open('C:/Users/lelik/Desktop/report.json', 'w', encoding='utf-8') as rep:
+        json.dump(data, rep, indent=4)
 
-with open('../report.json', 'w', encoding='utf8') as rep:
-    json.dump(data, rep, indent=4)
+task3(data, todos)
